9e72aa9e8a2c64895c51d1c338bc19534e029d3e
Marta <m.sebastian.val@gmail.com>
2026-01-30 14:39:24 +0100

    fix: fixed return type for price response

--- sales-simulator/controller/src/main/java/com/msv/controller/entity/PriceResponse.java
+++ sales-simulator/controller/src/main/java/com/msv/controller/entity/PriceResponse.java
@@ -1,5 +1,6 @@
 package com.msv.controller.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,8 +17,10 @@ public class PriceResponse {
 
     private String productId;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endDate;
 
     private double price;
